77364ca7dda54c69201d02237ea3cc78719717a5
Ryan Phillips <rcphillips@Dans-iMac-2.local>
2015-05-07 14:04:11 -0700

    Here comes some python code!

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 13:47:31 2015
+
+@author: rcphillips
+"""
+print "Trying to get this to work"
+
+A = [1,2,3]
+
+for each in A:
+    if each == 1:
+        print each
+    else:
+        print "nope"
+        
+print "okay fuck it, I'm using spyder and not ipython notebook"
+
